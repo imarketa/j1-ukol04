5189e941ef8ea97abdb3614d1c81d1a9c75fc237
iMarketa <marketa.kleger@gmail.com>
2022-05-18 16:14:53 +0200

    opravené sedmé číslo z osudí

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -9,44 +9,43 @@ import java.util.stream.IntStream;
  * Třída pro losování Sportky.
  */
 public class Sportka {
-  private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>();
 
-  public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-      for (int i=1;i<50;i++){
-          osudi.add(i);
-      }
-  }
+    public Sportka() {
+        //TODO naplnit osudí čísly 1 až 49.
+        for (int i = 1; i < 50; i++) {
+            osudi.add(i);
+        }
+    }
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
-  public void zamichat() {
-    //TODO zamíchat osudí
-      Collections.shuffle(osudi);
-  }
+    /**
+     * Zamíchá osudí.
+     *
+     * @see Collections#shuffle(List)
+     */
+    public void zamichat() {
+        //TODO zamíchat osudí
+        Collections.shuffle(osudi);
+    }
 
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
-  public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return osudi.subList(0,6);
-  }
+    /**
+     * Vrátí seznam s 6 vylosovanými čísly.
+     *
+     * @return Seznam vylosovaných čísel.
+     * @see List#subList(int, int)
+     */
+    public List<Integer> vylosovanaCisla() {
+        //TODO Vrátit seznam prvních 6 čísel z osudí.
+        return osudi.subList(0, 6);
+    }
 
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
-  public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return osudi.get(6);
-  }
+    /**
+     * Vrátí sedmé vylosované číslo (dodatkové číslo)
+     *
+     * @return Dodatkové číslo.
+     */
+    public Integer dodatkoveCislo() {
+        //TODO Vrátit sedmé číslo z osudí.
+        return osudi.get(6);
+    }
 }
